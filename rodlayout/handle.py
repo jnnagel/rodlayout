0bda5ba1b3bebcf08976275ea9d879f668290968
Julian Nagel <julian_nagel@gmx.de>
2020-01-10 17:08:08 +0100

    Changed align handles to match those in the simple geometry package.

--- rodlayout/handle.py
+++ rodlayout/handle.py
@@ -1,18 +1,13 @@
 _handle_part_y = [
     ('top', 'upper'),
-    ('upper', 'upper'),
     ('bottom', 'lower'),
-    ('lower', 'lower'),
     ('center', 'center'),
 ]
 _handle_part_x = ['left', 'center', 'right']
 
 python_skill_handle = {
-    f'{py}_{x}': f'{sy}{x.title()}' for py, sy in _handle_part_y for x in _handle_part_x
+    # Designed to use the same handles as the simple geometry package.
+    f'{py}_{x}': f'{sy}{x.title()}'
+    for py, sy in _handle_part_y
+    for x in _handle_part_x
 }
-
-for x in _handle_part_x:
-    python_skill_handle[x] = 'center' + x.title()
-
-for py, sy in _handle_part_y:
-    python_skill_handle[py] = sy + 'Center'
